6f7a374c119d04ce41c6ddf485a48c129c9db3ba
jkrward <j.k.ward@ncl.ac.uk>
2020-01-07 14:08:05 +0000

    adding density plot function

---
+++ R/density_plot.R
@@ -0,0 +1,18 @@
+
+#' Density plots
+#'
+#' @param v the object to be plotted
+#'
+#' @return
+#' @export
+#'
+#' @examples
+density_plot <- function(v){
+  # this won't work without ggplot2
+
+  stopifnot(is.numeric(v))
+
+  data.frame(values = v) %>%
+    ggplot(aes(x = values))+
+    geom_density()
+}
