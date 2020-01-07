74bc19acfca6e831d5d169c36ee6f64d9c5f58ec
jkrward <j.k.ward@ncl.ac.uk>
2020-01-07 14:20:02 +0000

    fixing imports

--- R/density_plot.R
+++ R/density_plot.R
@@ -4,12 +4,12 @@
 #' @param v the object to be plotted
 #'
 #' @return density plot
+#' @importFrom ggplot2 ggplot aes geom_density
+#' @importFrom magrittr %>%
 #' @export
 #'
 #' @examples
 density_plot <- function(v){
-  # this won't work without ggplot2
-
   stopifnot(is.numeric(v))
 
   data.frame(values = v) %>%
